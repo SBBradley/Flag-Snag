c9c01303d18963852bd485fd720cd7081db5f225
SBBradley <97514020+SBBradley@users.noreply.github.com>
2022-02-02 16:16:43 +0900

    Update Form1.cs

--- FlagSnag/Form1.cs
+++ FlagSnag/Form1.cs
@@ -8,8 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-/*A game that shows the flags of the world.
- *Date: February 2,2022
+/*A program that generates flags of the world.
+ *Date: February 2, 2022
  */
 namespace FlagSnag
 {
@@ -19,7 +19,8 @@ namespace FlagSnag
         {
             InitializeComponent();
         }
-
+        
+        //Click event for the single button on the form.
         private void button1_Click_1(object sender, EventArgs e)
         {
             
@@ -33,49 +34,37 @@ namespace FlagSnag
                 case "United States":
                     pictureBox1.Image = Properties.Resources.USA;
                     break;
-
                 case "Canada":
                     pictureBox1.Image = Properties.Resources.CANADA;
                     break;
-
                 case "Mexico":
                     pictureBox1.Image = Properties.Resources.MEXICO;
                     break;
-
                 case "Japan":
                     pictureBox1.Image = Properties.Resources.JAPAN;
                     break;
-
                 case "South Korea":
                     pictureBox1.Image = Properties.Resources.SOUTHKOREA;
                     break;
-
                 case "China":
                     pictureBox1.Image = Properties.Resources.CHINA;
                     break;
-
                 case "Germany":
                     pictureBox1.Image = Properties.Resources.GERMANY;
                     break;
-
                 case "United Kingdom":
                     pictureBox1.Image = Properties.Resources.UNITEDKINGDOM;
                     break;
-
                 case "France":
                     pictureBox1.Image = Properties.Resources.FRANCE;
                     break;
-
                 case "Russia":
                     pictureBox1.Image = Properties.Resources.RUSSIA;
                     break;
-
-
                 default:
                     MessageBox.Show("Error: Invalid country name entered.");
                     break;
             }
-
         }
     }
 }
